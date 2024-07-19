b7f6c6a14a076cfd98ca641fe15bf925a5379f89
KailanySousa <kailany-sousa2010@hotmail.com>
2024-07-19 19:53:57 -0300

    feat: criando componente de confirmar exclusão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './pensamentos/pages/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './pensamentos/pages/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'listar' },
   { path: 'criar', component: CriarPensamentoComponent },
   { path: 'listar', component: ListarPensamentoComponent },
+  { path: 'excluir/:id', component: ExcluirPensamentoComponent}
 ];
 
 @NgModule({
