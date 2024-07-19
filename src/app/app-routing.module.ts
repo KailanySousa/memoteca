f0be69684b6d7bbf6c22d16cf1b32dbf10d257f0
KailanySousa <kailany-sousa2010@hotmail.com>
2024-07-19 17:43:56 -0300

    feat: configurar rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'listar' },
+  { path: 'criar', component: CriarPensamentoComponent },
+  { path: 'listar', component: ListarPensamentoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
