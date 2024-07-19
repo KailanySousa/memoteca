50b5f0d670c7d051714854032035f2e6a85adb23
KailanySousa <kailany-sousa2010@hotmail.com>
2024-07-19 17:59:14 -0300

    feat: criar componente para exibir os pensamentos ja cadastrados

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+  listaPensamentos = [
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Dev',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Só sei que nada sei',
+      autoria: 'Sócrates',
+      modelo: 'modelo3'
+    }
+  ];
+
 }
